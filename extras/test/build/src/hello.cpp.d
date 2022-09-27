8d7fd9321adfc3367055085b63682097189faebd
Miroslav Talasek <miroslav.talasek@gmail.com>
2022-09-27 10:11:01 +0200

    clean mess

--- extras/test/build/src/hello.cpp.d
+++
@@ -1,7 +0,0 @@
-build/./src/hello.cpp.o: src/hello.cpp \
- /home/miro/git/ArduinoJson/src/ArduinoJson.h \
- /home/miro/git/ArduinoJson/src/ArduinoJson.hpp \
- /home/miro/git/ArduinoJson/src/ArduinoJson/Configuration.hpp
-/home/miro/git/ArduinoJson/src/ArduinoJson.h:
-/home/miro/git/ArduinoJson/src/ArduinoJson.hpp:
-/home/miro/git/ArduinoJson/src/ArduinoJson/Configuration.hpp:
